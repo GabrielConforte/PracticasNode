541c524229317740217ca078a078ac86e8b5948a
GabrielConforte <82458582+GabrielConforte@users.noreply.github.com>
2022-01-20 17:32:54 -0300

    desafío 3

---
+++ contenedor.js
@@ -0,0 +1,105 @@
+/*
+    Implementar programa que contenga una clase llamada contened
+    el nombre del archivo con el que va a trabajar e implemente los siguinetes
+    metodos
+
+    save(Object): number - Recibe un objeto y lo guarda en el archivo, devuelve el id del objeto
+    getById(number): Object - Recibe un id y devuelve el objeto que corresponde o null si no existe
+    getAll(): Object[] - Devuelve un arreglo con todos los objetos
+    deleteById(number): void - Recibe un id y elimina el objeto que corresponde o false si no existe
+    deleteAll(): void - Elimina todos los objetos
+*/
+
+
+const fs = require('fs');
+
+class Contenedor {
+
+    //guarda un objeto en el archivo
+    async save(objeto) {
+        try{
+            let contenedor = await this.getAll();
+                contenedor = JSON.parse(contenedor);
+                //agrega un id al objeto a partir del id del ultimo objeto de contenedor
+                const max = contenedor.reduce((a,b) => a.id > b.id ? a:b, {id: 0} )
+                objeto.id = max.id + 1;
+                contenedor.push(objeto);
+                let json = JSON.stringify(contenedor);
+                await fs.promises.writeFile('./contenedor.txt', json);
+            return objeto.id;
+        }catch(error){
+            console.log(error);
+        }
+    }
+
+    //devuelve un objeto por id
+    async getById(id) {
+        try{
+            let contenedor = await this.getAll();
+            contenedor = JSON.parse(contenedor);
+            let objeto = contenedor.find(obj => obj.id == id);
+
+            if(objeto!=undefined){
+                return objeto;
+            }
+            else{
+                return 'no existe el objeto';
+            }
+        }catch(error){
+            console.log(error);
+        }
+        
+    }
+
+    //devuelve un arreglo con todos los objetos
+    async getAll() { if (!fs.existsSync('./contenedor.txt')) {
+        fs.promises.writeFile('./contenedor.txt', '[]');
+    }try{
+        let dato = await fs.promises.readFile('./contenedor.txt', 'utf8')
+        return dato;
+    }catch(error){
+        console.log(error);
+    }
+    }
+
+    //elimina un objeto por id
+    async deleteById(id) {
+        let contenedor = await this.getAll();
+        contenedor = JSON.parse(contenedor);
+        let objeto = contenedor.find(obj => obj.id == id);
+        contenedor = contenedor.filter(obj => obj.id != id);
+        if(objeto!=undefined){
+            let json = JSON.stringify(contenedor);
+            fs.promises.writeFile('./contenedor.txt', json);
+            console.log('objeto eliminado');}
+        else{
+            console.log('no existe el objeto');
+        }
+    }
+
+    //elimina todos los objetos
+    async deleteAll() {
+        await fs.promises.writeFile('./contenedor.txt', '[]');
+        console.log('todos los objetos fueron eliminados');
+    }
+}
+
+
+async function main() {
+    //envia los metodos a main.js
+    const contenedor = new Contenedor();
+    const data = await contenedor.getAll();
+
+
+}
+
+main();
+
+// importa todos los metodos
+module.exports = {
+    save: Contenedor.prototype.save,
+    getById: Contenedor.prototype.getById,
+    getAll: Contenedor.prototype.getAll,
+    deleteById: Contenedor.prototype.deleteById,
+    deleteAll: Contenedor.prototype.deleteAll
+}
--- main.js
+++ main.js
@@ -1,15 +1,21 @@
+
+//envuelve esto en un main asincrono
+
+
 const express = require('express');
 const moment = require('moment');
-const app = express();//crea una instancia de express, lo inicializa
-const PORT = 9000;
+const contenedor = require('./contenedor');
+const app = express();
+const PORT = 3000;
 let contador = 0;
 app.get("/", (req, res, next) => {
     console.log(req)
-    res.send("<h1 style='color:blue'> hola mundo </h1>");
+    res.send("<h1 style='color:blue'> hola mundillo </h1>");
     });
 app.get("/visitas",(req,res,next)=>{
     contador++;
     res.send(`Hoy tuvimos ${contador} visitas`);
+
     });
 
 app.get("/fyh", (req, res, next) => {
@@ -20,6 +26,31 @@ app.get("/fyh", (req, res, next) => {
 const server = app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+
+app.get("/productos", (req, res, next) => {
+    contenedor.getAll().then(data => {
+        let productos = JSON.parse(data);
+        res.send(`<h1>Productos</h1>
+        <ul>
+            ${productos.map(producto => `<li>${producto.title} $${producto.price}</li>`).join('')}
+        </ul>`);
+
+    });
+
+});
+            
+//la pagina productosRandom debe entregar un producto al azar del archivo contenedor.txt
+app.get("/productosRandom", (req, res, next) => {
+    contenedor.getAll().then(data => {
+        let productos = JSON.parse(data);
+        let random = Math.floor(Math.random() * productos.length);
+        res.send(`<h1>Producto al azar</h1>
+        <ul>
+            <li>${productos[random].title} $${productos[random].price}</li>
+        </ul>`);
+    });
+}); 
 
 server.on("error", error => console.log(error));
 
