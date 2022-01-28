111a41b2916d01d8915697ebf1e342e11e89fff2
GabrielConforte <82458582+GabrielConforte@users.noreply.github.com>
2022-01-28 17:50:09 -0300

    entrega

--- public/js/main.js
+++ public/js/main.js
@@ -25,12 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 routerApi.get('/', (req, res, next) =>  {
     contenedor.getAll().then(data => {
         let productos = JSON.parse(data);
-        res.send(`<h1>Productos</h1>
-        <ul>
-            ${productos.map(producto => `<li>${producto.title} $${producto.price}</li>`).join('')}
-        </ul>
-        <a href="/">Volver</a></button>`);
-
+        res.json(productos);
     });
 
 });
@@ -40,44 +35,50 @@ routerApi.get('/:id', (req, res, next) => {
 
     contenedor.getById(req.params.id).then(data => {
         if(data.id!=undefined){
-        res.send(`<li>${data.title} $${data.price}</li>
-        <a href="/">Volver</a></button>
-        `);}
+            res.json(data);}
         else{
-            res.json({ error : 'producto no encontrado' }
-        `<a href="/">Volver</a></button>`);
+            res.json({ error : 'producto no encontrado' });
         }
-        
     });
 });
 
 routerApi.post('/', (req, res, next) => {
     let producto = req.body;
     contenedor.save(producto).then(data => {
-        res.send(`<h1>Producto agregado</h1>
-        <li>${producto.title} $${producto.price}</li>
-        <a href="/">Volver</a></button>`);
+        res.json({atencion: "producto guardado"});
     });
 });
-
 
 //put -> recibe y actualiza un producto según su id.
 routerApi.put('/:id', (req, res, next) => {
     let producto = req.body;
     contenedor.update(req.params.id, producto).then(data => {
-        res.send(`<h1>Producto actualizado</h1>
-        <li>${producto.title} $${producto.price}</li>
-        <a href="/">Volver</a></button>`);
+        res.json(data);
     });
 });
 
 routerApi.delete('/:id', (req, res, next) => {
     contenedor.deleteById(req.params.id).then(data => {
-        res.send(`<h1>Producto eliminado</h1>
-        <a href="/">Volver</a></button>`);
+        res.json({ atencion: 'producto eliminado' });
     });
 });
 app.use('/api/productos', routerApi);
+
+
+//mensajes para errores del servidor
+app.use((err, req, res, next) => {
+    res.status(500).json({ error: err.message });
+});
+app.use((req, res, next) => {
+    res.status(404).json({ error: '404 - not found' });
+});
+app.use((err, req, res, next) => {
+    res.status(401).json({ error: '401 - not authorized' });
+});
+app.use((err, req, res, next) => {
+    res.status(403).json({ error: '403 - forbidden' });
+});
+
 
 server.on("error", error => console.log(error));
 
